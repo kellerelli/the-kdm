a339405531012a4f4520b71601463ab73b6bd152
jcafarelli <jcafarelli@roamdata.com>
2017-01-24 17:21:59 -0500

    This commit is def broken. inital start to dynamo

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative 'application'
 
+AWS_SETTINGS = YAML.load_file("#{Rails.root}/config/aws_dynamo.yml")
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Aws.init
---
+++ config/initializers/aws.rb
@@ -0,0 +1,2 @@
+#This is to make AWS avalable on the entire project
+require 'aws'
